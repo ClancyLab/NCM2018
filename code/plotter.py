bdf24f85e10329a196a3749ff9c598a14b50cc2f
Henry Herbol <hherbol@gmail.com>
2018-06-16 11:21:09 -0400

    Changed plotter.py to plot in kj/mol instead of kT_300

--- code/plotter.py
+++ code/plotter.py
@@ -16,6 +16,8 @@ elif sys.version_info[0] == 3:
 else:
     raise Exception("Error - Unknown python version!")
 
+KT300_to_KJMOL = 2.4942796
+
 
 def plot(stats, se_scale=2.0):
     '''
@@ -34,6 +36,11 @@ def plot(stats, se_scale=2.0):
         mu = mu * -1.0
         mu = mu[offset[key]:]
         se = se[offset[key]:]
+
+        # Unit conversion of kT_300 to kj/mol
+        mu = mu * KT300_to_KJMOL
+        se = se * KT300_to_KJMOL
+
         plt.plot(range(offset[key], N_POINTS), mu, label=key, linewidth=3)
         plt.fill_between(range(offset[key], N_POINTS), mu - se_scale * se, mu + se_scale * se, alpha=0.5)
 
@@ -42,20 +49,20 @@ def plot(stats, se_scale=2.0):
     plt.yticks(fontsize=12)
 
     plt.xlim([1, 50])
-    plt.ylim([-42, -37])
+    plt.ylim([-42 * KT300_to_KJMOL, -37 * KT300_to_KJMOL])
 
     plt.xlabel("Number of Observations", fontsize=16)
-    plt.ylabel("Intermolecular Binding Energy (kT at 300 K)", fontsize=16)
+    plt.ylabel("Intermolecular Binding Energy (kJ/mol)", fontsize=16)
 
     plt.gca().invert_yaxis()
 
     # Make a section for our training set region (if we had one)
     if offset["PAL"] > 0:
         xvals = np.arange(0, offset["PAL"] + 1)
-        yvals1 = [10 for x in xvals]
-        yvals2 = [-100 for x in xvals]
+        yvals1 = [10 * KT300_to_KJMOL for x in xvals]
+        yvals2 = [-100 * KT300_to_KJMOL for x in xvals]
         plt.fill_between(xvals, yvals1, yvals2, facecolor='gray', alpha=0.3)
-        plt.text(2, -40, "PAL Training\n    Region")
+        plt.text(2, -40 * KT300_to_KJMOL, "PAL Training\n    Region")
 
     if not os.path.exists("out"):
         os.mkdir("out")
