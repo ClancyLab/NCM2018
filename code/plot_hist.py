51dc71ded55e73b934f85c5e98372675e4f2d44d
Henry Herbol <hherbol@gmail.com>
2018-06-26 17:30:03 -0400

    Hist plotter

---
+++ code/plot_hist.py
@@ -0,0 +1,32 @@
+import cPickle as pickle
+import matplotlib
+matplotlib.use("Agg")
+import matplotlib.pyplot as plt
+
+fig = plt.figure(figsize=(10, 8))
+
+# Only use the following line if you have enough ram to handle the random data
+names = ["random", "smac_ord", "smac", "simple", "pal", "pal_0"]
+# names = ["smac_ord", "smac", "simple", "pal", "pal_0"]
+
+for i, path in enumerate(names):
+    pal = pickle.load(open("../data/out/best_%s.dat" % path, 'r'))
+    dist = [list(p).index(p[-1]) for p in pal]
+
+    print min(dist), max(dist), path, len(dist)
+
+    ax = plt.subplot(len(names), 1, i + 1)
+
+    # the bins should be of integer width, because poisson is an integer distribution
+    entries, bin_edges, _ = ax.hist(dist, label=path, bins=25, range=[-0.5, 240.5], normed=True)
+
+    plt.legend(loc="center left", bbox_to_anchor=[1.0, 0.5], shadow=True, fancybox=True)
+    plt.ylim(0, 0.055)
+
+plt.subplots_adjust(left=0.1, right=0.85, top=0.9, bottom=0.1)
+fig.text(0.2, 0.95, "Histogram of Iterations to Global Convergence", fontsize=18)
+fig.text(0.5, 0.02, 'Number of Iterations', ha='center', fontsize=16)
+fig.text(0.02, 0.5, 'Normalized Probability', va='center', rotation='vertical', fontsize=16)
+
+# plt.show()
+plt.savefig("Hist_new.png")
